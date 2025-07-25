27a602b1f816f880694c3266661b24e7bc45aa68
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-07-25 16:21:26 -0400

    señales(feat):se agrego logica con signal en counter-page

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.html',
@@ -6,12 +6,15 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
   counter = 10;
+  counterSignal = signal(10);
 
   increaseBy(value: number) {
     this.counter += value;
+    this.counterSignal.update((current) => current + value);
   }
 
   resetCounter() {
-    this.counter = 10;
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
