91692c3ebf891be1ce01ddbc2488e8ba5bc8824b
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-07-29 15:07:58 -0400

    mejora: se agregó la estrategia de detección de cambios OnPush en CounterPageComponent

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.html',
   styleUrl: './counter-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
   counter = 10;
   counterSignal = signal(10);
+
+  /* constructor() {
+    setInterval(() => {
+      this.counter += 1;
+      this.counterSignal.update((v)=> v + 1);
+      this.increaseBy(1);
+      console.log('Tick');
+    }, 2000);
+  } */
 
   increaseBy(value: number) {
     this.counter += value;
