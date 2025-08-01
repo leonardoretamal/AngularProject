693e434d8505c4e56c1ae2ed8d19a4e2c23ba8bd
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-08-01 12:25:23 -0400

    feat: mejorar estructura y estilos en HeroPageComponent

--- src/app/pages/hero/hero-page.ts
+++ src/app/pages/hero/hero-page.ts
@@ -1,16 +1,29 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 
 @Component({
   templateUrl: './hero-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [UpperCasePipe],
 })
 export class HeroPageComponent {
   name = signal('ironman');
   age = signal(45);
 
-  getHeroDescription() {
-    return `Hero: ${this.name()} - ${this.age()}`;
-  }
+  heroDescription = computed(() => {
+    const description = `${this.name()} - ${this.age()}`;
+    return description;
+  });
+
+  capitalizeName = () => {
+    const name = this.name().toUpperCase();
+    return name;
+  };
 
   changeHero() {
     this.name.set('spiderman');
@@ -25,6 +38,4 @@ export class HeroPageComponent {
   changeAge() {
     this.age.set(60);
   }
-
-  
 }
