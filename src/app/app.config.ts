67fd2cdcc3323a74ef14bd88121f51f103c97a69
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-08-14 13:47:00 -0400

    Despliegues y HashRouter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 // Configuración de la aplicación Angular
 // Aquí se definen los proveedores y la configuración de la aplicación
@@ -9,6 +14,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+
+    //HashStrategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 };
